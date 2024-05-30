e5563363a962987362c816eff4ecc38cb00958da
NaryshArt <168580312+NaryshArt@users.noreply.github.com>
2024-05-30 22:57:50 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -2,25 +2,36 @@ const darkModeToggle = document.getElementById('dark-mode-toggle');
 const body = document.body;
 const rate = document.getElementById('rating_part');
 
-// Проверяем, есть ли сохраненное состояние темной темы в localStorage
 const isDarkModeEnabled = localStorage.getItem('darkModeEnabled') === 'true';
 
-// Устанавливаем начальное состояние темной темы
 if (isDarkModeEnabled) {
     body.classList.add('dark-mode');
     rate.classList.add('dark-mode');
 }
 
-// Обработчик клика на переключатель
 darkModeToggle.addEventListener('click', function() {
     body.classList.toggle('dark-mode');
     rate.classList.toggle('dark-mode');
 
-// Сохраняем состояние темной темы в localStorage
     localStorage.setItem('darkModeEnabled', body.classList.contains('dark-mode'));
 });
 
 window.addEventListener('load', function () {
     const loader = document.querySelector('.loader');
     loader.classList.add('hidden');
+});
+
+let valueDisplays = document.querySelectorAll(".ani_count");
+let interval = 4000;
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
 });
